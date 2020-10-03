2c09a1234e6bbbf9ac9e065de37bd3d9a4979680
Synx-x <siyandamtamo@gmail.com>
2020-10-03 21:09:27 +0200

    final project

---
+++ customer/my_account.php
@@ -0,0 +1,325 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Connections SA</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+   
+   <div id="top"><!-- Top Begin -->
+       
+       <div class="container"><!-- container Begin -->
+           
+           <br>
+           <div class="col-md-6"><!-- col-md-6 Begin -->
+               
+               <ul class="menu"><!-- cmenu Begin -->
+                   
+                 
+                   
+                   
+               </ul><!-- menu Finish -->
+               
+           </div><!-- col-md-6 Finish -->
+           
+       </div><!-- container Finish -->
+       
+   </div><!-- Top Finish -->
+   
+   <div id="navbar" class="navbar navbar-default"><!-- navbar navbar-default Begin -->
+       
+       <div class="container"><!-- container Begin -->
+           
+           <div class="navbar-header"><!-- navbar-header Begin -->
+               
+               <a href="../index.php" class="navbar-brand home"><!-- navbar-brand home Begin -->
+                   
+                   <img src="images/ecom-store-logo.png" alt="M-dev-Store Logo" class="hidden-xs">
+                   <img src="images/ecom-store-logo-mobile.png" alt="M-dev-Store Logo Mobile" class="visible-xs">
+                   
+               </a><!-- navbar-brand home Finish -->
+               
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                   
+                   <span class="sr-only">Toggle Navigation</span>
+                   
+                   <i class="fa fa-align-justify"></i>
+                   
+               </button>
+               
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                   
+                   <span class="sr-only">Toggle Search</span>
+                   
+                   <i class="fa fa-search"></i>
+                   
+               </button>
+               
+           </div><!-- navbar-header Finish -->
+           
+           <div class="navbar-collapse collapse" id="navigation"><!-- navbar-collapse collapse Begin -->
+               
+               <div class="padding-nav"><!-- padding-nav Begin -->
+                   
+                   <ul class="nav navbar-nav left"><!-- nav navbar-nav left Begin -->
+                       
+                       <li>
+                           <a href="../index.php">Home</a>
+                       </li>
+                       <li>
+                           <a href="../shop.php">Browse</a>
+                       </li>
+                       <li>
+                           <a href="../contact.php">Contact Us</a>
+                       </li>
+                       <li>
+                       <a href="customer_register.php">Register</a>
+                   </li>
+               <li>
+                       <a href="../checkout.php">Login</a>
+                   </li>
+                   <li>
+                       <a href="checkout.php">My Account</a>
+                   </li>
+                       
+                   </ul><!-- nav navbar-nav left Finish -->
+                   
+               </div><!-- padding-nav Finish -->
+               
+               <a href="../cart.php" class="btn navbar-btn btn-primary right"><!-- btn navbar-btn btn-primary Begin -->
+                   
+                   <i class="fa fa-shopping-cart"></i>
+                   
+                   <span> Your Cart</span>
+                   
+               </a><!-- btn navbar-btn btn-primary Finish -->
+               
+               <div class="navbar-collapse collapse right"><!-- navbar-collapse collapse right Begin -->
+                   
+                   <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search"><!-- btn btn-primary navbar-btn Begin -->
+                       
+                       <span class="sr-only">Toggle Search</span>
+                       
+                       <i class="fa fa-search"></i>
+                       
+                   </button><!-- btn btn-primary navbar-btn Finish -->
+                   
+               </div><!-- navbar-collapse collapse right Finish -->
+               
+               <div class="collapse clearfix" id="search"><!-- collapse clearfix Begin -->
+                   
+                   <form method="get" action="results.php" class="navbar-form"><!-- navbar-form Begin -->
+                       
+                       <div class="input-group"><!-- input-group Begin -->
+                           
+                           <input type="text" class="form-control" placeholder="Search" name="user_query" required>
+                           
+                           <span class="input-group-btn"><!-- input-group-btn Begin -->
+                           
+                           <button type="submit" name="search" value="Search" class="btn btn-primary"><!-- btn btn-primary Begin -->
+                               
+                               <i class="fa fa-search"></i>
+                               
+                           </button><!-- btn btn-primary Finish -->
+                           
+                           </span><!-- input-group-btn Finish -->
+                           
+                       </div><!-- input-group Finish -->
+                       
+                   </form><!-- navbar-form Finish -->
+                   
+               </div><!-- collapse clearfix Finish -->
+               
+           </div><!-- navbar-collapse collapse Finish -->
+           
+       </div><!-- container Finish -->
+       
+   </div><!-- navbar navbar-default Finish -->   
+   <div id="content"><!-- #content Begin -->
+       <div class="container"><!-- container Begin -->
+           <div class="col-md-12"><!-- col-md-12 Begin -->
+               
+               <ul class="breadcrumb"><!-- breadcrumb Begin -->
+                   <li>
+                       <a href="index.php">Home</a>
+                   </li>
+                   <li>
+                       My Account
+                   </li>
+               </ul><!-- breadcrumb Finish -->
+               
+           </div><!-- col-md-12 Finish -->
+           
+           <div class="col-md-3"><!-- col-md-3 Begin -->
+   
+   <div class="panel panel-default sidebar-menu"><!--  panel panel-default sidebar-menu Begin  -->
+    
+    <div class="panel-heading"><!--  panel-heading  Begin  -->
+        
+        <center><!--  center  Begin  -->
+            
+            <img src="customer_images/erika.jpg" alt="Mdev Profile">
+            
+        </center><!--  center  Finish  -->
+        
+        <br/>
+        
+        <h3 align="center" class="panel-title"><!--  panel-title  Begin  -->
+            
+            Name: Angry Doge
+            
+        </h3><!--  panel-title  Finish -->
+        
+    </div><!--  panel-heading Finish  -->
+    
+    <div class="panel-body"><!--  panel-body Begin  -->
+        
+        <ul class="nav-pills nav-stacked nav"><!--  nav-pills nav-stacked nav Begin  -->
+            
+            <li class="">
+                
+                <a href="my_account.php?my_orders">
+                    
+                    <i class="fa fa-list"></i> My Orders
+                    
+                </a>
+                
+            </li>
+            
+            <li class="">
+                
+            
+                
+            </li>
+            
+            <li class="">
+                
+                <a href="my_account.php?edit_account">
+                    
+                    <i class="fa fa-pencil"></i> Edit Account
+                    
+                </a>
+                
+            </li>
+            
+            <li class="">
+                
+                <a href="my_account.php?change_pass">
+                    
+                    <i class="fa fa-user"></i> Change Password
+                    
+                </a>
+                
+            </li>
+            
+            <li class="active">
+                
+                <a href="my_account.php?delete_account">
+                    
+                    <i class="fa fa-trash-o"></i> Delete Account
+                    
+                </a>
+                
+            </li>
+            
+            <li>
+                
+                <a href="logout.php">
+                    
+                    <i class="fa fa-sign-out"></i> Log Out
+                    
+                </a>
+                
+            </li>
+            
+        </ul><!--  nav-pills nav-stacked nav Begin  -->
+        
+    </div><!--  panel-body Finish  -->
+    
+</div><!--  panel panel-default sidebar-menu Finish  -->               
+           </div><!-- col-md-3 Finish -->
+           
+           <div class="col-md-9"><!-- col-md-9 Begin -->
+               
+               <div class="box"><!-- box Begin -->
+                   
+                                      
+                   
+                   
+                                      
+                                      
+                   <center><!-- center Begin -->
+    
+    <h1> Do You Realy Want To Delete Your Account ? </h1>
+    
+    <form action="" method="post"><!-- form Begin -->
+        
+       <input type="submit" name="Yes" value="Yes, I Want To Delete" class="btn btn-danger"> 
+        
+       <input type="submit" name="No" value="No, I Dont Want To Delete" class="btn btn-primary"> 
+        
+    </form><!-- form Finish -->
+    
+</center><!-- center Finish -->                   
+               </div><!-- box Finish -->
+               
+           </div><!-- col-md-9 Finish -->
+           
+       </div><!-- container Finish -->
+   </div><!-- #content Finish -->
+   
+   <div id="footer"><!-- #footer Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="row"><!-- row Begin -->
+           
+            
+           
+            
+            
+            <div class="col-sm-6 col-md-3">
+                
+               
+              
+                     
+           
+                
+              
+                
+            </div>
+        </div><!-- row Finish -->
+    </div><!-- container Finish -->
+</div><!-- #footer Finish -->
+
+
+<div id="copyright"><!-- #copyright Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-left">&copy; 2020 ConnectionsSA All Rights Reserve</p>
+            
+        </div><!-- col-md-6 Finish -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+            
+        </div><!-- col-md-6 Finish -->
+    </div><!-- container Finish -->
+</div><!-- #copyright Finish -->    
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+    
+    
+</body>
+</html>
